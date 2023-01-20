6412c3c368fa659143865724546485f526216844
Pranjal Goyal <100211916+dontwike@users.noreply.github.com>
2023-01-20 22:51:33 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,17 +1,55 @@
+import { useState } from 'react';
 import './App.css';
+import Alert from './components/Alert';
 import Navbar from './components/Navbar';
-// import TextArea from './components/TextArea';
+import TextArea from './components/TextArea';
 import About from './components/About';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom";
 
 function App() {
+
+  const[mode, setMode] = useState("light"); // Weather dark mode is enabled or disabled
+  const[alert, setAlert] = useState(null);
+
+  const showAlert = (message, type) => {
+    setAlert ({
+      message: message,
+      type: type
+    });
+
+    setTimeout(() => {
+      setAlert(null);
+    }, 2000);
+  };
+
+  const toggleMode = () => {
+    if ( mode === "dark" ) {
+      setMode("light");
+      document.body.style.backgroundColor = "white";
+      showAlert("Light Mode turned on Successfully", "success");
+    } else if ( mode === "light" ) {
+      setMode("dark");
+      document.body.style.backgroundColor = "black";
+      showAlert("Dark Mode turned on Successfully", "success");
+    }
+  };
+
   return (
     <>
-    <Navbar title="TextUtils"/>
-    
+    <Router>
+    <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode}/>
+    <Alert alert={alert}/>
     <div className='container my-5'>
-      {/* <TextArea heading='Enter the text Here'/> */}
-      <About />
+      <Routes>
+          <Route exact path="/about" element={<About />}/>
+          <Route exact path="/" element={<TextArea heading='Enter the text Here' mode={mode} showAlert={showAlert}/>}/>
+      </Routes>
       </div>
+    </Router>
     </>
   );
 }
